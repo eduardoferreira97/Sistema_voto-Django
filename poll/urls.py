5decf1a8eedcfa09493970263ee08aff6b658e9e
eduardoferreira97 <animedudu12345@gmail.com>
2022-12-05 11:39:44 -0300

    With views to see details

--- poll/urls.py
+++ poll/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = 'poll'
 urlpatterns = [
+    # /poll/
     path('', views.index, name='index'),
+    # ex: /poll/3/
+    path('specifics/<int:question_id>/', views.detail, name='detail'),
+    # ex: /poll/3/results/
+    path('<int:question_id>/results/',views.results,name='results'),
+    # ex: /poll/3/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- poll/views.py
+++ poll/views.py
@@ -1,6 +1,23 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpRequest("Olá, mundo.Você está na página de votação.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = { 'latest_question_list': latest_question_list,}
+    return render(request, 'poll/index.html',context)
+
+def detail(request, question_id):
+    
+    question = get_object_or_404(Question,pk=question_id)
+
+    return render(request, 'poll/detail.html',{'question': question})
+
+def results(request, question_id):
+    responde = "You're loking at the results of questions %s"
+
+def vote(request, question_id):
+    return HttpRequest("You're voting on question %s." % question_id)
